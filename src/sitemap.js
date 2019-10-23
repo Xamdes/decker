d4804602c615e6cbe35aea96b247557b49f2e25e
Steven Colburn <stevencolburn@xamdes.com>
2019-10-23 04:10:17 -0700

    can build sitemap

---
+++ src/sitemap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router';
+require('babel-register');
+const Sitemap = require('react-router-sitemap').default;
+
+const router = (
+  <Switch>
+    <Route exact path="/" />
+    <Route path="/cyberdeck" />
+    <Route path="/Error404" />
+  </Switch>
+);
+
+new Sitemap(router).build('https://cyber-ex.net').save('./build/sitemap.xml');
