e85379763fb89ee8384c76281aa9c89e4994690f
Steven Colburn <6747482+Xamdes@users.noreply.github.com>
2024-06-09 02:17:05 -0700

    Update RiggerRules.jsx
    Fixed spelling mistake

--- src/components/RiggerRules.jsx
+++ src/components/RiggerRules.jsx
@@ -16,7 +16,7 @@ function RiggerRules(){
       <TextSection className="bg-info">
         <p>Jumping in to a vehicle or device requires the following: A Control Rig, Ownership or 3 marks for the vehicle/drone, and the vahicle/drone has a rigger interface</p>
         <p>Jumping in is a Complex Action if in AR or a Simple Actiuon if you are in VR</p>
-        <p>If you are using a direct connection and already plugged into the vehcile or RCC then you can jump directly via a Simple Action.</p>
+        <p>If you are using a direct connection and already plugged into the Vehicle or RCC then you can jump directly via a Simple Action.</p>
         <p>Vehicle actions are considered matrix actions while jumped in, so any bonuses to matrix actions are also bonuses to Vehicle Control Tests, Gunnery Tests, and Sensor Tests.</p>
         <p>Biofeedback is resisted with (Willpower+Firewall) and the rigger is resisting half the damage, rounded up, done to the Vehicle/Drone.</p>
         <p>Matrix damage is done to whatever device is used when making the jump. So if commlink is used then that is what takes the damage. If dierectly connected to a vehicle then the vehicle takes matrix damage.</p>
@@ -28,7 +28,7 @@ function RiggerRules(){
             <li>Control Rig Rating is added to the Vehicle Limits: (Sensor, Speed, Handling, and Accuracy for mounted weapons)</li>
             <li>Control Rig Rating in extra device to Vehicle Skill Tests</li>
             <li>Control Rig Rating reduces the Threshold for Vehicle Tests to a minimum of 1</li>
-            <li>Treat vehicle actions as Matrix actions: (Vehcile Control Tests, Gunnery Tests, and Sensor Tests)</li>
+            <li>Treat vehicle actions as Matrix actions: (Vehicle Control Tests, Gunnery Tests, and Sensor Tests)</li>
             <li>For example shooting a mounted weapon would be the following: Gunnery + Logic + Control Rig Rating [Accuracy + Control Rig Rating ]</li>
           </ul>
         </p>
@@ -40,7 +40,7 @@ function RiggerRules(){
             <li>Control Rig Rating is added to the Vehicle Limits: (Sensor, Speed, Handling, and Accuracy for mounted weapons)</li>
             <li>Control Rig Rating in extra device to Vehicle Skill Tests</li>
             <li>Control Rig Rating reduces the Threshold for Vehicle Tests to a minimum of 1</li>
-            <li>Treat vehicle actions as Matrix actions: (Vehcile Control Tests, Gunnery Tests, and Sensor Tests)</li>
+            <li>Treat vehicle actions as Matrix actions: (Vehicle Control Tests, Gunnery Tests, and Sensor Tests)</li>
             <li>+2 dice on all Matrix actions: (Normal Bonus for Hot Sim)</li>
             <li>For example shooting a mounted weapon would be the following: Gunnery + Logic + Control Rig Rating + 2 Hot Sim Bonus [Accuracy + Control Rig Rating ]</li>
           </ul>
